6322a6841dd0fd8c8d70c9300bbb925aebfb05a0
gabryelboeira <gabryel392@gmail.com>
2022-01-20 09:02:26 -0300

    initial commit application

---
+++ crud-cidade/src/main/java/br/edu/utfpr/cp/espjava/crudcidade/CrudCidadeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.cp.espjava.crudcidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCidadeApplication.class, args);
+	}
+
+}
---
+++ crud-cidade/src/test/java/br/edu/utfpr/cp/espjava/crudcidade/CrudCidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.cp.espjava.crudcidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
