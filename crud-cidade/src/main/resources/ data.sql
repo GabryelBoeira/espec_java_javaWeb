5d967f7bac43d339c9bcf492a2686a72bb099c30
GabryelBoeira <gabryel@asaplog.com.br>
2022-03-14 16:56:16 -0300

    Atualização por problema nos acessos no arquivo sql

--- crud-cidade/src/main/resources/ data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `usuario` VALUES (1, 'john', '$2a$10$AMutjWtC94r5BzrsjbBTg.bovIKkhW2fKJCpK/7FTIEEjBToM4nq2');
-INSERT INTO `usuario` VALUES (2, 'anna', '$2a$10$AMutjWtC94r5BzrsjbBTg.bovIKkhW2fKJCpK/7FTIEEjBToM4nq2');
-
-INSERT INTO `usuario_nivel_list` VALUES (1, 'listar');
-INSERT INTO `usuario_nivel_list` VALUES (2, 'listar');
-INSERT INTO `usuario_nivel_list` VALUES (2, 'admin');
